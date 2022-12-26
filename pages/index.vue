ced4269b0f7cdfdfa8189339d6bcc9d2313ee2cb
Auricdhero <datauric@gmail.com>
2022-12-26 12:07:59 +0000

    changing color

--- pages/index.vue
+++ pages/index.vue
@@ -50,7 +50,7 @@ export default{
 body {
     margin: 0;
     font-weight: 100;
-    background: radial-gradient(#29C2E9);
+    background: radial-gradient(#29C2E9,#166790);
     -webkit-overflow-Y: hidden;
     -moz-overflow-Y: hidden;
     -o-overflow-Y: hidden;
