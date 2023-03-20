f0659230714abb7eceb278bda9a13ac0c17192b3
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-03-20 22:11:25 +0000

    added something to Welcome page

--- components/biodata.vue
+++ components/biodata.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        
+       <v-container>
+        <v-card style="padding: 1rem;">
+            <h3 class="text-center">Please Note: All fields marked with * are mandatory.</h3>
+            <v-form>
+                <h3>Basic Information</h3>
+                <v-divider></v-divider>
+
+                <br>
+                <v-row>
+                    <v-label>Title</v-label>
+                    <v-select label="Please Select"></v-select>
+                </v-row>
+            </v-form>
+        </v-card>
+       </v-container>
     </div>
 </template>
