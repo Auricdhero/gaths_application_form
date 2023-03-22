192975b004d4c6074ee3c4561c7781cf6b1d5ec9
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-03-22 21:14:22 +0000

    adding biodata

--- components/biodata.vue
+++ components/biodata.vue
@@ -1,19 +1,141 @@
 <template>
-    <div>
-       <v-container>
-        <v-card style="padding: 1rem;">
-            <h3 class="text-center">Please Note: All fields marked with * are mandatory.</h3>
-            <v-form>
-                <h3>Basic Information</h3>
-                <v-divider></v-divider>
+  <div>
+    <v-container>
+      <v-card elevation="2" style="padding: 1rem" color="blue">
+        <h3 style="color: white">Biodata</h3> </v-card
+      ><br />
+      <v-card style="padding: 1rem">
+        <h3 class="text-center">
+          Please Note: All fields marked with * are mandatory.
+        </h3>
+        <v-form>
+          <h3>Basic Information</h3>
+          <v-divider></v-divider>
 
-                <br>
-                <v-row>
-                    <v-label>Title</v-label>
-                    <v-select label="Please Select"></v-select>
-                </v-row>
-            </v-form>
-        </v-card>
-       </v-container>
-    </div>
+          <br />
+          <v-row>
+            <v-col>
+              <v-label>Title</v-label>
+              <v-select label="Please Select" outlined></v-select>
+            </v-col>
+
+            <v-col>
+              <v-label>Surname</v-label>
+              <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-label>First Name</v-label>
+              <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+            <v-col>
+              <v-label>Other Names</v-label>
+              <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-label>Gender</v-label>
+              <v-select label="Please Select" outlined></v-select>
+            </v-col>
+            <v-col>
+              <v-label>Date of Birth</v-label>
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="date"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                    outlined
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="date" no-title scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="menu = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="primary" @click="$refs.menu.save(date)">
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+                <v-label>Country of Birth</v-label>
+                <v-select label="Please Select" outlined></v-select>
+            </v-col>
+            <v-col>
+                <v-label>Nationality</v-label>
+                <v-select label="Please Select" outlined></v-select>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+                <v-label>Hometown</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+            <v-col>
+                <v-label>Region</v-label>
+                <v-select label="Please Select" outlined></v-select>
+            </v-col>
+          </v-row>
+
+          <br>
+
+          <h3 class="text-left">ADDRESS OF APPLICANT</h3>
+          <v-divider></v-divider><br>
+          <v-row>
+            <v-col>
+                <v-label>Address Line 1</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+            <v-col>
+                <v-label>Address Line 2</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+                <v-label>Ghana Card Number (If available)</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+            <v-col>
+                <v-label>Phone Number</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+                <v-label>Email</v-label>
+                <v-text-field label="Enter" outlined></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-btn color="success">Save and Continue</v-btn>
+        </v-form>
+      </v-card>
+    </v-container>
+  </div>
 </template>
+<script>
+export default {
+    data: () => ({
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu: false,
+      modal: false,
+      menu2: false,
+    }),
+}
+</script>
