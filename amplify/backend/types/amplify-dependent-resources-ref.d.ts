71033ef6ed0750f4d19c45da5c43a7c363001031
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-04-16 09:07:10 +0000

    adding new amplify configs

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,12 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "gathsapplicationform902680b2": {
+    "gathsapplicationform56cfff97": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
