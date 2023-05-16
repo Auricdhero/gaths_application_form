2c1ddb48d559c7c17a409c4df7efa7e6ed9b5450
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-05-16 18:40:57 +0000

    reconfigured auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "gathsapplicationform2b82b1a2": {
+    "gathsapplicationform2eb264eb2eb264eb": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
