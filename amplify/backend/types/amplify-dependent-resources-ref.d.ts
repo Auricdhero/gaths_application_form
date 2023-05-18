c21caf98d1d2449e92daed2ff2001e5a7a0a73b9
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-05-18 20:10:44 +0000

    adding graphql to backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "gathsapplicationform": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "gathsapplicationform2eb264eb2eb264eb": {
       "AppClientID": "string",
