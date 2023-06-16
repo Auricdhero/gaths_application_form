240e9f155bf43cb338e1a11fe908061da345d95a
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-06-16 13:46:22 +0000

    Update theFooter.vue

--- components/theFooter.vue
+++ components/theFooter.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <v-footer dark padless style="height: 5em">
-      <v-card>
-        <v-card-text class="py-2 white--text text-center">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-card-text>
-      </v-card>
+    <v-footer color="deep-purple accent-4" padless style="height: 5em">
+      <v-col cols="12">
+        <v-card elevation="0" color="deep-purple accent-4">
+          <v-card-text class="py-2 px-5 text-center white--text">
+            Copyright &copy;{{ new Date().getFullYear() }} GATHS,All rights
+            reserved.
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-footer>
   </div>
 </template>
