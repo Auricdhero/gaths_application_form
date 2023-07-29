e1fa2fc7331c8fe772874cb0aa1321be6ad65210
aurictorkornoo <44117263+Auricdhero@users.noreply.github.com>
2023-07-29 08:05:45 +0000

    adding datastore api

---
+++ src/models/index.d.ts
@@ -0,0 +1,87 @@
+import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from "@aws-amplify/datastore";
+// @ts-ignore
+import { LazyLoading, LazyLoadingDisabled } from "@aws-amplify/datastore";
+
+
+
+
+
+type Eageruser = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<user, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly title: string;
+  readonly surname: string;
+  readonly firstname: string;
+  readonly othername: string;
+  readonly gender: string;
+  readonly dob: string;
+  readonly country_of_birth: string;
+  readonly nationality: string;
+  readonly hometown: string;
+  readonly region: string;
+  readonly addressLine1: string;
+  readonly addressLine2?: string | null;
+  readonly ghCardNo?: string | null;
+  readonly phoneNo: string;
+  readonly email: string;
+  readonly nameOfInstitution: string;
+  readonly institutionRegion: string;
+  readonly Prog: string;
+  readonly level: string;
+  readonly dateStart: string;
+  readonly endDate: string;
+  readonly studentId: string;
+  readonly department: string;
+  readonly localChapterName: string;
+  readonly PositionsChapter: string;
+  readonly dateJoined: string;
+  readonly membershipNo: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+type Lazyuser = {
+  readonly [__modelMeta__]: {
+    identifier: ManagedIdentifier<user, 'id'>;
+    readOnlyFields: 'createdAt' | 'updatedAt';
+  };
+  readonly id: string;
+  readonly title: string;
+  readonly surname: string;
+  readonly firstname: string;
+  readonly othername: string;
+  readonly gender: string;
+  readonly dob: string;
+  readonly country_of_birth: string;
+  readonly nationality: string;
+  readonly hometown: string;
+  readonly region: string;
+  readonly addressLine1: string;
+  readonly addressLine2?: string | null;
+  readonly ghCardNo?: string | null;
+  readonly phoneNo: string;
+  readonly email: string;
+  readonly nameOfInstitution: string;
+  readonly institutionRegion: string;
+  readonly Prog: string;
+  readonly level: string;
+  readonly dateStart: string;
+  readonly endDate: string;
+  readonly studentId: string;
+  readonly department: string;
+  readonly localChapterName: string;
+  readonly PositionsChapter: string;
+  readonly dateJoined: string;
+  readonly membershipNo: string;
+  readonly createdAt?: string | null;
+  readonly updatedAt?: string | null;
+}
+
+export declare type user = LazyLoading extends LazyLoadingDisabled ? Eageruser : Lazyuser
+
+export declare const user: (new (init: ModelInit<user>) => user) & {
+  copyOf(source: user, mutator: (draft: MutableModel<user>) => MutableModel<user> | void): user;
+}
---
+++ src/models/schema.js
@@ -0,0 +1,249 @@
+export const schema = {
+    "models": {
+        "user": {
+            "name": "user",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "surname": {
+                    "name": "surname",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "firstname": {
+                    "name": "firstname",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "othername": {
+                    "name": "othername",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "gender": {
+                    "name": "gender",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "dob": {
+                    "name": "dob",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "country_of_birth": {
+                    "name": "country_of_birth",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "nationality": {
+                    "name": "nationality",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "hometown": {
+                    "name": "hometown",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "region": {
+                    "name": "region",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "addressLine1": {
+                    "name": "addressLine1",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "addressLine2": {
+                    "name": "addressLine2",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "ghCardNo": {
+                    "name": "ghCardNo",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "phoneNo": {
+                    "name": "phoneNo",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "email": {
+                    "name": "email",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "nameOfInstitution": {
+                    "name": "nameOfInstitution",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "institutionRegion": {
+                    "name": "institutionRegion",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "Prog": {
+                    "name": "Prog",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "level": {
+                    "name": "level",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "dateStart": {
+                    "name": "dateStart",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "endDate": {
+                    "name": "endDate",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "studentId": {
+                    "name": "studentId",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "department": {
+                    "name": "department",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "localChapterName": {
+                    "name": "localChapterName",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "PositionsChapter": {
+                    "name": "PositionsChapter",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "dateJoined": {
+                    "name": "dateJoined",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "membershipNo": {
+                    "name": "membershipNo",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "private",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "codegenVersion": "3.4.3",
+    "version": "9ab280d9caf60975947480660738dfca"
+};
